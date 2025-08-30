0ee701db4ac86c0ec9e6ebf28045cc346f729781
cuds <ana@r.c>
2025-08-30 22:23:21 +0200

    conserta typo no exercicio1.txt e adiciona comentarios nos codigos

--- aula3/exercicio/exercicio-rim/client/Main.java
+++ aula3/exercicio/exercicio-rim/client/Main.java
@@ -6,7 +6,8 @@ import calcRmi.Calc;
 public class Main {
 	public static void main(String[] args) {
 		try {
-			Calc objCalc = (Calc)Naming.lookup("//localhost:2004/Calc");
+			Calc objCalc = (Calc)Naming.lookup("//localhost:2004/Calc"); 
+			//Busca o ip localhost na porta 2004 pelo Objeto Calc
 			System.out.println("O resultado da soma é : " + objCalc.add(3, 7));
 		} catch (Exception e) {
 			e.printStackTrace();
--- aula3/exercicio/exercicio-rim/server/Main.java
+++ aula3/exercicio/exercicio-rim/server/Main.java
@@ -7,11 +7,14 @@ import java.rmi.registry.LocateRegistry;
 class Main {
 	public static void main(String[] args){
 		try {
-		
-			LocateRegistry.createRegistry(2004);
+			//Usa a porta 2004 para receber chamadas
+			LocateRegistry.createRegistry(2004); 
 
 			CalcImpl objRMI = new CalcImpl();
+
+			//Ouve o IP localhost porta 2004 com a classe Calc
 			Naming.rebind("//127.0.0.1:2004/Calc", objRMI);
+
 		}	catch(Exception e) {
 			e.printStackTrace();
 		}
