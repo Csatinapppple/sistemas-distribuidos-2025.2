05db499c04acf47abad7ad82b8ed1dce0c9cb361
cuds <ana@r.c>
2025-09-24 04:14:54 +0200

    results

--- aula5/PingPongPy/udp/test_udp.sh
+++ aula5/PingPongPy/udp/test_udp.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # use test_udp.sh $IP_ADDRESS $PORT
-python3 udp_client.py $1 $2 100 >> results.csv # 100 bytes
-python3 udp_client.py $1 $2 500 >> results.csv # 500 bytes
-python3 udp_client.py $1 $2 1024 >> results.csv # 1kb
-python3 udp_client.py $1 $2 102400 >> results.csv # 100kb
-python3 udp_client.py $1 $2 512000 >> results.csv # 500kb
-python3 udp_client.py $1 $2 1048576 >> results.csv # 1mb
-python3 udp_client.py $1 $2 10485760 >> results.csv # 10mb
+python3 udp_client.py $1 $2 100 | tee -a results.csv # 100 bytes
+python3 udp_client.py $1 $2 500 | tee -a results.csv # 500 bytes
+python3 udp_client.py $1 $2 1024 | tee -a results.csv # 1kb
+python3 udp_client.py $1 $2 102400 | tee -a results.csv # 100kb
+python3 udp_client.py $1 $2 512000 | tee -a results.csv # 500kb
+python3 udp_client.py $1 $2 1048576 | tee -a results.csv # 1mb
+python3 udp_client.py $1 $2 10485760 | tee -a results.csv # 10mb
